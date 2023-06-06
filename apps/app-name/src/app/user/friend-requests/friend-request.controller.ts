de302b9515c1d7f1c0fb6fbd0197ae1009df1688
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-06-06 19:27:49 +0200

    refactor: friend requests url

--- apps/app-name/src/app/user/friend-requests/friend-request.controller.ts
+++ apps/app-name/src/app/user/friend-requests/friend-request.controller.ts
@@ -1,15 +1,14 @@
 import { JwtAuthGuard, RequestWithUser } from '@kittgen/auth';
-import { FriendRequest } from '@kittgen/user';
 import { Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from "@nestjs/common";
 import { FriendRequestUsecases } from "./friend-request.usecases";
 import { GetFriendRequestDto } from './get-friend-request.dto';
 
-@Controller()
+@Controller('friend-requests')
 export class FriendRequestsController {
   constructor(private readonly usecases: FriendRequestUsecases) {}
 
   @UseGuards(JwtAuthGuard)
-  @Post('/friend-requests/:username')
+  @Post(':username')
   async sendFriendRequest(
     @Req() req: RequestWithUser,
     @Param('username') toUserName: string
@@ -19,7 +18,7 @@ export class FriendRequestsController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('friend-requests/pending')
+  @Get('pending')
   async getPendingFriendRequests(
     @Req() req: RequestWithUser
   ): Promise<GetFriendRequestDto[]> {
@@ -27,7 +26,7 @@ export class FriendRequestsController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('friend-requests/sent')
+  @Get('sent')
   async getSentFriendRequests(
     @Req() req: RequestWithUser
   ): Promise<GetFriendRequestDto[]> {
@@ -35,18 +34,17 @@ export class FriendRequestsController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Patch('friend-requests/:friendRequestId')
+  @Patch(':friendRequestId')
   async confirmFriendRequest(@Param('friendRequestId') friendRequestId: string) {
     return this.usecases.confirmRequest(friendRequestId);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Delete('friend-requests/:friendRequestId')
+  @Delete(':friendRequestId')
   async cancelFriendRequest(
     @Req() req: RequestWithUser,
     @Param('friendRequestId') friendRequestId: string
   ): Promise<void> {
     return this.usecases.cancelFriendRequest(req.user, friendRequestId);
   }
-
 }
