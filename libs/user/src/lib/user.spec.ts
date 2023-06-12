7a6df6a1cfa452f896d342b39be27bcfa26de345
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-06-12 15:01:33 +0200

    test: fix tests

--- libs/user/src/lib/user.spec.ts
+++ libs/user/src/lib/user.spec.ts
@@ -14,8 +14,7 @@ describe('User', () => {
       profile: Profile.create({ username: 'lisa' }),
     })
     fry.sendFriendRequestTo(lisa)
-    expect(fry.sentFriendRequests).toHaveLength(1)
-    expect(lisa.receivedFriendRequests).toHaveLength(1)
+    expect(fry.friendRequests).toHaveLength(1)
   })
 
   it('should allow confirming a friend request', () => {
@@ -31,10 +30,8 @@ describe('User', () => {
     })
     const friendRequest= fry.sendFriendRequestTo(lisa)
     friendRequest.accept()
-    expect(fry.sentFriendRequests).toHaveLength(0)
-    expect(lisa.receivedFriendRequests).toHaveLength(0)
+    expect(fry.friendRequests).toHaveLength(0)
     expect(fry.friends).toHaveLength(1)
-    expect(lisa.friends).toHaveLength(1)
   });
 
   it('should allow cancelling a friend request', () => {
@@ -50,12 +47,8 @@ describe('User', () => {
     })
     const friendRequest = fry.sendFriendRequestTo(lisa)
     friendRequest.cancel()
-    expect(fry.sentFriendRequests).toHaveLength(0)
-    expect(lisa.receivedFriendRequests).toHaveLength(0)
+    expect(fry.friendRequests).toHaveLength(0)
     // TODO: provide getter
     expect(fry.friends).toBeUndefined();
-    expect(lisa.friends).toBeUndefined();
-    // expect(fry.friends).toHaveLength(0)
-    // expect(lisa.friends).toHaveLength(0)
   });
 })
