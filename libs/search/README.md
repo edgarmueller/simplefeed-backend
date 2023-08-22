92b820208678e755198cf93b80255b85b04a32c2
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-08-22 17:22:30 +0200

    feat: fuzzy user search

--- libs/search/README.md
+++ libs/search/README.md
@@ -5,3 +5,12 @@ This library was generated with [Nx](https://nx.dev).
 ## Running unit tests
 
 Run `nx test elastic` to execute the unit tests via [Jest](https://jestjs.io).
+
+## FAQ
+
+### How can I list the index?
+`curl -X GET http://localhost:9200/users/_search\?q\=$SEARCHTERM | jq .`
+
+### How can I delete users from the index?
+
+`curl -X DELETE http://localhost:9200/users/_doc/$USER_ID`
--- libs/search/src/lib/search.service.ts
+++ libs/search/src/lib/search.service.ts
@@ -1,44 +1,76 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '@simplefeed/user';
-import { ElasticsearchService } from '@nestjs/elasticsearch';
-import { UserSearchDto } from './dto/user-search.dto';
-import { UserSearchResultDto } from './dto/user-search-result.dto';
- 
+import { Injectable, Logger } from '@nestjs/common'
+import { User } from '@simplefeed/user'
+import { ElasticsearchService } from '@nestjs/elasticsearch'
+import { UserSearchDto } from './dto/user-search.dto'
+import { UserSearchResultDto } from './dto/user-search-result.dto'
+
 @Injectable()
 export class SearchService {
+  private readonly logger = new Logger(SearchService.name)
   index = 'users'
- 
-  constructor(
-    private readonly elasticsearchService: ElasticsearchService
-  ) {
-  }
- 
+
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
   async indexUser(user: User) {
-    return await this.elasticsearchService.index<UserSearchResultDto, UserSearchDto>({
+    this.logger.log(`Indexing user ${JSON.stringify(user, null, 1)}`)
+    return await this.elasticsearchService.index<
+      UserSearchResultDto,
+      UserSearchDto
+    >({
       index: this.index,
       body: {
         id: user.id,
         username: user.profile.username,
-				firstName: user.profile.firstName,
-				lastName: user.profile.lastName,
+        firstName: user.profile.firstName,
+        lastName: user.profile.lastName,
         imageUrl: user.profile.imageUrl,
-      }
+      },
     })
   }
- 
+
   async searchUser(text: string) {
-    const { body } = await this.elasticsearchService.search<UserSearchResultDto>({
-      index: this.index,
-      body: {
-        query: {
-          multi_match: {
-            query: text,
-            fields: ['username', 'firstName', 'lastName']
-          }
-        }
-      }
-    })
-    const hits = body.hits.hits;
-    return hits.map((item) => item._source);
+    const { body } =
+      await this.elasticsearchService.search<UserSearchResultDto>({
+        index: this.index,
+
+        body: {
+          query: {
+            bool: {
+              should: [
+                {
+                  multi_match: {
+                    query: text,
+                    fields: ['username', 'firstName', 'lastName'],
+                  },
+                },
+                {
+                  bool: {
+                    should: [
+                      {
+                        fuzzy: {
+                          firstName: {
+                            value: text,
+                            fuzziness: 'AUTO',
+                          },
+                        },
+                      },
+                      {
+                        fuzzy: {
+                          lastName: {
+                            value: text,
+                            fuzziness: 'AUTO',
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        },
+      })
+    const hits = body.hits.hits
+    return hits.map((item) => item._source)
   }
 }
