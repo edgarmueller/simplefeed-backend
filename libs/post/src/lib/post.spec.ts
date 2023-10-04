2cb87b49cd7ec08433acc56d702a27a6126b0478
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-10-04 10:58:12 +0200

    test: add specs for likes

--- libs/post/src/lib/post.spec.ts
+++ libs/post/src/lib/post.spec.ts
@@ -1,3 +1,39 @@
+import { User, Profile } from '@simplefeed/user'
+import { Post } from './post'
+
+
 describe('Post', () => {
-  it('should be tested', () => {})
+  it('can be liked only once by an user', () => {
+    const fry = User.create({
+      email: 'fry@example.com',
+      password: 'secret',
+      profile: Profile.create({ username: 'fry' }),
+    })
+    const post = Post.create({
+      body: 'content',
+      author: fry
+    })
+    post.like(fry)
+    post.like(fry)
+    expect(post.getUncommittedEvents()).toHaveLength(1)
+    expect(post.likes).toHaveLength(1)
+    expect(post.likes[0].unliked).toBe(false)
+  })
+
+  it('can be unliked only by an user', () => {
+    const fry = User.create({
+      email: 'fry@example.com',
+      password: 'secret',
+      profile: Profile.create({ username: 'fry' }),
+    })
+    const post = Post.create({
+      body: 'content',
+      author: fry
+    })
+    post.like(fry)
+    post.unlike(fry)
+    expect(post.getUncommittedEvents()).toHaveLength(2)
+    expect(post.likes).toHaveLength(1)
+    expect(post.likes[0].unliked).toBe(true)
+  })
 })
